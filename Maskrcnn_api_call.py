f99adfc63233b8a7b9a02c1e6d504944400386c8
Sofiane23i <medjram.sofiane@gmail.com>
2025-06-16 04:44:03 +0100

    return image false api

--- Maskrcnn_api_call.py
+++ Maskrcnn_api_call.py
@@ -3,11 +3,11 @@
 
 # 2) Pure JSON example (Python requests)
 import json, base64, requests
-with open("car.jpg", "rb") as f:
+with open("../car2.jpg", "rb") as f:
     payload = {
         "image": base64.b64encode(f.read()).decode(),
-        "return_image": True
+        "return_image": False
     }
-r = requests.post("http://192.168.118.56:5000/api/predict",
+r = requests.post("http://192.168.112.56:5000/api/predict",
                   json=payload, timeout=120)
 print(json.dumps(r.json(), indent=2))
